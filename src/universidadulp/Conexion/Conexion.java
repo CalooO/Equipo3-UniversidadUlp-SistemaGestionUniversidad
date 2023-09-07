1e9504838aa4b4b2033bd44d113a6e841aa8ec5c
SergioKazmi <skazmiruk@outlook.com>
2023-09-07 15:54:29 -0300

    Creación de proyecto

---
+++ src/universidadulp/Conexion/Conexion.java
@@ -0,0 +1,34 @@
+package universidadulp.Conexion;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:mariadb://localhost:3306/", DB = "universidadulp", USUARIO = "root", PASSWORD = "";
+    private static Connection connection;
+
+    private Conexion() {
+    }
+
+    public static Connection getConexion() {
+        if (connection == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "No se pudo cargar el driver");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error de conexión");
+
+            }
+        }
+        return connection;
+
+    }
+
+
+}
--- src/universidadulp/UniversidadUlp.java
+++ src/universidadulp/UniversidadUlp.java
@@ -14,23 +14,7 @@ public class UniversidadUlp {
 
    
     public static void main(String[] args) {
-        //con nombre de la base de datos
-        Conexion conexion = new Conexion("Universidadulp"); 
-        conexion.conectar();
         
-        Alumno alumno1= new Alumno(36314380, "Redondo", "Paula", LocalDate.of(1992, Month.OCTOBER, 7), true);
-        alumno1.agregarAlumno(alumno1);
-        
-//        String sql= "INSERT INTO alumno VALUES (null, 36314380, 'Redondo', 'Paula', '1992-10-07',true)";
-//        
-//        PreparedStatement ps;
-//        try {
-//            ps = conexion.conectar().prepareStatement(sql);
-//            ps.executeQuery();
-//        } catch (SQLException ex) {
-//           JOptionPane.showMessageDialog(null,"Error en la consulta");
-//        }
-//        
     
    }
       
